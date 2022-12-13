40a7b52e20d6b3e4d14f0b2e3cd6e573101f0567
Ha Manh Do <hadm.haui@gmail.com>
2022-12-13 13:08:49 +0700

    Filtering Data

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
+use App\Services\V1\CustomerQuery;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
@@ -15,10 +17,18 @@ class CustomerController extends Controller
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
+     * @example http://localhost/api/v1/customers?postalCode[lt]=82730
      */
-    public function index()
+    public function index(Request $request)
     {
-        return new CustomerCollection(Customer::paginate());
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request); // ['column', 'operator', 'value']
+
+        if (count($queryItems) == 0) {
+            return new CustomerCollection(Customer::paginate());
+        }
+
+        return new CustomerCollection(Customer::where($queryItems)->paginate());
     }
 
     /**
