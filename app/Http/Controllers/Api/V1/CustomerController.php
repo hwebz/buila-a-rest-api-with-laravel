b69f1a585f0590f08e5065af68aa54f6bff1cf7c
Ha Manh Do <hadm.haui@gmail.com>
2022-12-13 18:26:17 +0700

    Updating with PUT and PATCH

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
@@ -90,10 +90,12 @@ class CustomerController extends Controller
      * @param  \App\Http\Requests\UpdateCustomerRequest  $request
      * @param  \App\Models\Customer  $customer
      * @return \Illuminate\Http\Response
+     * @example PUT \api\v1\customers
+     * @example PATCH \api\v1\customers
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
