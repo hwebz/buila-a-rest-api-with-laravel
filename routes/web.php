a3e6a0921f2271a3f36fb86cecc33f5b7366266a
Ha Manh Do <hadm.haui@gmail.com>
2022-12-14 01:27:32 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,8 @@ Route::get('/setup', function() {
 
             $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
             $updateToken = $user->createToken('update-token', ['create', 'update']);
-            $basicToken = $user->createToken('basic-token');
+            // empty param gonna make basic token with ['*'] and by default allows all actions
+            $basicToken = $user->createToken('basic-token', ['none']); 
 
             return [
                 'admin' => $adminToken->plainTextToken,
